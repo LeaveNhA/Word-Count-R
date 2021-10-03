fa598ffbef638adf55987ca0f410ccf76512647c
Seçkin KÜKRER <seckin.kukrer@hotmail.com>
2021-10-03 11:11:24 +0300

    Re-factor for more functional approach.

--- word-count.R
+++ word-count.R
@@ -1,11 +1,15 @@
-word_count <- function(input) {
-  lower_input               <- tolower(input)
-  numeric_list              <- utf8ToInt(lower_input)
-  filtered_list             <- Filter(function(x) x %in% c(32, 48:57, 65:90, 97:122), numeric_list)
-  filtered_string           <- intToUtf8(filtered_list)
-  splitted_words            <- strsplit(filtered_string, "[ \t\n]")[[1]]
-  filtered_splitted_words   <- Filter(function(w) nchar(w) > 0, splitted_words)
-  freq                      <- table(filtered_splitted_words)
+combine <- function(fns){
+  function(args)
+    Reduce(function(fn, acc) fn(acc), rev(fns), args, right = TRUE)
+}
 
-  as.list(freq)
+word_count <- function(input) {
+  combine(c(tolower,
+            utf8ToInt,
+            function(numeric_list) Filter(function(x) x %in% c(32, 48:57, 65:90, 97:122), numeric_list),
+            intToUtf8,
+            function(filtered_string) strsplit(filtered_string, "[ \t\n]")[[1]],
+            function(splitted_words) Filter(function(w) nchar(w) > 0, splitted_words),
+            table,
+            as.list))(input)
 }
